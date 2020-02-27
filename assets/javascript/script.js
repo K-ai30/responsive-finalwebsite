19dc9a9dd6f126549bc868d8dcff4b8edf7d17d2
K-ai30 <artpirate11@aim.com>
2020-02-27 16:37:03 -0500

    added new images and revised css

---
+++ assets/javascript/script.js
@@ -0,0 +1,11 @@
+// $(document).ready(function(){
+// // object-fit polyfill run
+// objectFitImages();
+
+// /* init Jarallax */
+// jarallax(document.querySelectorAll('.jarallax'));
+
+// jarallax(document.querySelectorAll('.jarallax-keep-img'), {
+//     keepImg: true,
+// });
+// })
