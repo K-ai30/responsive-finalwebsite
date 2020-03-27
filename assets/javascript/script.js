86e42c9ef4c1c19fb1b30a6694e30f3b1a65b3fc
K-ai30 <artpirate11@aim.com>
2020-03-27 13:45:24 -0400

    updated portfolio images and project links

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,11 +1,15 @@
-// $(document).ready(function(){
-// // object-fit polyfill run
-// objectFitImages();
+// Get the button
+let scrollBtn = $(".scrollUp");
 
-// /* init Jarallax */
-// jarallax(document.querySelectorAll('.jarallax'));
 
-// jarallax(document.querySelectorAll('.jarallax-keep-img'), {
-//     keepImg: true,
-// });
-// })
+$(document).ready(function() {
+    scrollBtn.on("click", function(event) {
+        topFunction(event);
+    })
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
