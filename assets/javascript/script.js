f1168cc69964fb1df2d6b9393f8a2b6bcf4974a5
K-ai30 <artpirate11@aim.com>
2020-06-06 02:32:52 -0400

    updated portfolio and links and added js/jquery scrollToTop functionality

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,15 +1,25 @@
 // Get the button
 let scrollBtn = $(".scrollUp");
+// scrollBtn = document.getElementsByClassName("scrollUp");
 
+// $(document).ready(function() {
+//     scrollBtn.on("click", function(event) {
+//         topFunction(event);
+//     })
+// }
 
-$(document).ready(function() {
-    scrollBtn.on("click", function(event) {
-        topFunction(event);
-    })
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollBtn.style.display = "block";
+    } else {
+        scrollBtn.style.display = "none";
+    }
 }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-  }
+}
